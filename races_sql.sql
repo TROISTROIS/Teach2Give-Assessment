3989f642e82ada1de5b1ea482f00ae21aff8c0e3
TROISTROIS <franciscangeno1@gmail.com>
2024-10-29 07:17:26 +0300

    Added comments

--- races_sql.sql
+++ races_sql.sql
@@ -1,3 +1,8 @@
+-- Write a query that will display the results below (Note: some columns might be renamed
+-- but use the column names above). It should only show 2020 data and order by driver
+-- points.
+
+
 -- DROP SCHEMA IF EXISTS F1 CASCADE;
 -- create schema
 CREATE SCHEMA IF NOT EXISTS F1;

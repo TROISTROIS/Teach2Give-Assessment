020705fecb54d3167f478ce2bebcb9c9b0475447
TROISTROIS <franciscangeno1@gmail.com>
2024-10-29 07:15:45 +0300

    Added comments

--- palindrome.py
+++ palindrome.py
@@ -1,3 +1,7 @@
+# Write a Python function that checks whether a word or phrase is palindrome or not.
+# Note: A palindrome is word, phrase, or sequence that reads the same
+# backward as forward, e.g., madam,kayak,racecar, or a phrase "nurses run"
+
 words = ["madam", "orange", "kayak", "Monday", "teacher", "racecar", "nursesrun", "abcde"]
 
 
--- pangram.py
+++ pangram.py
@@ -1,3 +1,9 @@
+# Write a Python function to check whether a string is pangram or not. (Assume
+# the string passed in does not have any punctuation)
+# Note: Pangrams are words or sentences containing every letter of the
+# alphabet at least once. For example: "The quick brown fox jumps over the
+# lazy dog"
+
 from string import ascii_lowercase as lower
 def is_pangram(my_string):
     return set(lower)-set(my_string.lower()) == set([])
